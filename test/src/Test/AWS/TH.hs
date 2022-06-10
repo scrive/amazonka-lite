bb31eec0e9bf2397a79cd8993d046e4fa811a317
Jan Synáček <jan.synacek@scrive.com>
2022-06-10 17:44:42 +0200

    Fix test compilation - implement liftTyped for Lift instances

--- test/src/Test/AWS/TH.hs
+++ test/src/Test/AWS/TH.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, CPP, RankNTypes, KindSignatures, PolyKinds #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans  #-}
 
@@ -12,13 +12,25 @@
 --
 module Test.AWS.TH where
 
-import           Control.Applicative
 import           Data.Time
+import           GHC.Exts (RuntimeRep, TYPE)
 import           Language.Haskell.TH
 import           Language.Haskell.TH.Syntax
 import           Network.AWS.Data.Text
 import           Network.AWS.Data.Time
 import           Network.AWS.Lens           (view)
+
+#if __GLASGOW_HASKELL__ > 900
+
+liftTyped' :: forall (r :: RuntimeRep) (a :: TYPE r) m. Quote m => m Exp -> Code m a
+liftTyped' = unsafeCodeCoerce
+
+#else
+
+liftTyped' :: forall (r :: RuntimeRep) (a :: TYPE r). Q Exp -> Q (TExp a)
+liftTyped' = unsafeTExpCoerce
+
+#endif
 
 mkTime :: Text -> Q Exp
 mkTime x =
@@ -28,15 +40,19 @@ mkTime x =
 
 instance Lift (Time a) where
     lift (Time x) = AppE (ConE (mkName "Time")) <$> lift x
+    liftTyped x = liftTyped' (lift x)
 
 instance Lift UTCTime where
     lift (UTCTime x y) = do
         x' <- lift x
         y' <- lift y
         return $ AppE (AppE (ConE (mkName "UTCTime")) x') y'
+    liftTyped x = liftTyped' (lift x)
 
 instance Lift DiffTime where
     lift x = [|toEnum $(lift (fromEnum x))|]
+    liftTyped x = liftTyped' (lift x)
 
 instance Lift Day where
     lift x = [|toEnum $(lift (fromEnum x))|]
+    liftTyped x = liftTyped' (lift x)
